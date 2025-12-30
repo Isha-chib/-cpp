09f87a8ac835ce7d02e0797e838a40482f6bdb4f
Isha Chib <ishachib641@gmail.com>
2025-12-30 21:58:02 +0530

    Add files via upload

---
+++ swapalternate.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n ;
+    cin>>n;
+
+    int arr[n];
+    for (int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    for (int i=0;i<n-1; i+=2)
+    {
+        if(i+1<n)
+        {
+            swap(arr[i],arr[i+1]);
+
+        }
+    }
+
+        for (int i=0; i<n-1; i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+}
